34e3a91a1fc4f85307365c1afe4786b0bdaab633
soo982181 <soo982181@naver.com>
2024-01-17 22:19:28 +0900

    database upload

--- dml.sql
+++ dml.sql
@@ -617,3 +617,18 @@ where id in (
 select u.user_id, count(p.id) from tbl_user u left outer join tbl_post p
 on u.id = p.user_id
 group by u.user_id;
+
+create view view_target_field_trip as
+    (select * from tbl_field_trip
+where id in (
+    select ft.id from tbl_apply a
+    right outer join tbl_field_trip ft
+    on a.field_trip_id = ft.id
+    group by ft.id
+    having count(child_id) < (
+    select floor(avg(ac.apply_count)) from (
+        select field_trip_id, count(child_id) apply_count from tbl_apply
+        group by field_trip_id) ac)
+));
+
+select * from view_target_field_trip;
